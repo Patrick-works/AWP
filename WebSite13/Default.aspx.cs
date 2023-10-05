b6862f88fe5edeb4cd04496a0b77782f36d08dd7
Patrick batman <138879144+Patrick-works@users.noreply.github.com>
2023-10-05 12:53:31 +0530

    Add files via upload

---
+++ WebSite13/Default.aspx.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.IO;
+using System.Xml;
+public partial class _Default : System.Web.UI.Page
+{
+    protected void Page_Load(object sender, EventArgs e)
+    {
+
+    }
+    protected void Button1_Click(object sender, EventArgs e)
+    {
+
+    }
+    Console                                                                                                                                                                    
+}
+
